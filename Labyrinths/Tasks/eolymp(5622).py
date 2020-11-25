d4dfc7225bf613c70db9933cfe2ec86b8b6c1c96
Invalid-coder <gusevvovik@gmail.com>
2020-11-25 23:37:59 +0200

    Added new problems

--- Labyrinths/Tasks/eolymp(5622).py
+++ Labyrinths/Tasks/eolymp(5622).py
@@ -1,6 +1,10 @@
+#https://www.e-olymp.com/uk/submissions/7842686
+
 WALL = '#'
 SPACE = '.'
 ENTRANCE = '+'
+di = [1, 0, -1, 0]
+dj = [0, 1, 0, -1]
 
 def inputMaze(N):
     maze = []
@@ -26,9 +30,33 @@ def inputMaze(N):
 
     return maze
 
+def count_area(maze):
+    n = len(maze)
+    exit = (n - 2, n - 2)
+    entrance = (1, 1)
+    queue = [entrance, exit]
+    visited = [[False for j in range(n)] for i in range(n)]
+    visited[entrance[0]][entrance[1]] = True
+    visited[exit[0]][exit[1]] = True
+    counter = 0
+
+    while queue:
+        i, j = queue.pop(0)
+
+        for k in range(len(dj)):
+            i1 = i + di[k]
+            j1 = j + dj[k]
+
+            if maze[i1][j1] == WALL:
+                counter += 1
+            elif maze[i1][j1] == SPACE and not visited[i1][j1]:
+                queue.append((i1, j1))
+                visited[i1][j1] = True
+
+    return counter * 9
+
 if __name__ == '__main__':
     n = int(input())
     maze = inputMaze(n)
 
-    for row in maze:
-        print(*row)
+    print(count_area(maze))
