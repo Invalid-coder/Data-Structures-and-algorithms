7ff89e98ac6616e4eb2b33fe42e4e62e38265e54
Vova Gusev <gusevvovik@gmail.com>
2021-04-09 12:33:32 +0300

    Added new tasks

---
+++ Arrays/Tasks/leetcode(867).py
@@ -0,0 +1,26 @@
+"""
+Given a 2D integer array matrix, return the transpose of matrix.
+
+The transpose of a matrix is the matrix flipped over its main diagonal,
+switching the matrix's row and column indices.
+
+Example 1:
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[1,4,7],[2,5,8],[3,6,9]]
+
+        for i in range(len(matrix)):
+            for j in range(i + 1, len(matrix)):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        return matrix
+"""
+
+class Solution:
+    def transpose(self, matrix):
+        R, C = len(matrix), len(matrix[0])
+        ans = [[matrix[j][i] for j in range(R)] for i in range(C)]
+        return ans
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.transpose([[1,2,3],[4,5,6]]))
